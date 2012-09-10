29b03ea1a2e38411dcacf4dc4fe330658d15b1f4
Kai Hossbach <paranoiax89@googlemail.com>
2012-09-10 19:30:12 +0200

    Beta 1.2.6
    -added fifth level

---
+++ src/levels/level5.lua
@@ -0,0 +1,20 @@
+	boundaries = 3400
+	playerX, playerY = 45,100
+	
+	addSensor(20,160,60,40)
+	addSensor(720,260,120,40)
+	addSensor(860,100,120,100)
+	addSensor(1080,280,120,40)
+	addSensor(1300,280,120,40)
+	addSensor(1520,280,120,40)
+	addSensor(1520,500,120,40)
+	
+	addWall(0,0,680,80)
+	addWall(0,220,680,80)
+	addWall(1020,60,20,320)
+	addWall(1240,60,20,320)
+	addWall(1040,360,200,20)
+	addWall(1260,360,200,20)
+	addWall(1460,60,20,540)
+	addWall(1680,60,20,540)
+	addWall(1480,580,200,20)
